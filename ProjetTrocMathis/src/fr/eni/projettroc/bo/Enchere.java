51953989bcd0bf00aa580c0c04b2ebe23b5cf15e
ebellec2021 <ebellec2021@42SE1-HF0455J.ad.campus-eni.fr>
2021-03-30 10:32:56 +0200

    Bo categorie encheres retraits

---
+++ ProjetTrocMathis/src/fr/eni/projettroc/bo/Enchere.java
@@ -0,0 +1,12 @@
+package fr.eni.projettroc.bo;
+
+import java.time.LocalDate;
+
+public class Enchere {
+private int no_enchere;
+private LocalDate date_enchere;
+private int montant_enchere;
+private Article article;
+private Utilisateur utilisateur;
+
+}
---
+++ ProjetTrocMathis/src/fr/eni/projettroc/bo/Retrait.java
@@ -0,0 +1,9 @@
+package fr.eni.projettroc.bo;
+
+public class Retrait {
+private Article article;
+private String rue;
+private String code_postal;
+private String ville;
+
+}
