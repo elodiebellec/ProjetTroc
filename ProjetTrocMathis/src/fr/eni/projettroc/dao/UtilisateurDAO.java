0c7d7c20972d39b31c1cd7bdad5f61e0a8315c59
fcormant2021 <fcormant2021@42SE1-JG0455J.ad.campus-eni.fr>
2021-03-30 16:57:31 +0200

    Gestion des conflits

--- ProjetTrocMathis/src/fr/eni/projettroc/dao/UtilisateurDAO.java
+++ ProjetTrocMathis/src/fr/eni/projettroc/dao/UtilisateurDAO.java
@@ -1,20 +1,14 @@
 package fr.eni.projettroc.dao;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> branch 'enchere' of https://github.com/elodiebellec/ProjetTroc.git
 import fr.eni.projettroc.exception.BusinessException;
 import fr.eni.projettroc.bo.Utilisateur;
 
 public interface UtilisateurDAO {
-<<<<<<< HEAD
+
 	
 public Utilisateur find(String pseudo, String mot_de_passe, String email)throws BusinessException;
-=======
->>>>>>> branch 'enchere' of https://github.com/elodiebellec/ProjetTroc.git
 
-<<<<<<< HEAD
 public Utilisateur selectByNoUtilisateur(int no_utilisateur)throws BusinessException;
 	
 }
@@ -23,13 +17,3 @@ public Utilisateur selectByNoUtilisateur(int no_utilisateur)throws BusinessExcep
 
 
 
-=======
-	
-	Utilisateur find(String pseudo, String mot_de_passe, String email)throws BusinessException;
-	
-    public void insertUtilisateur(Utilisateur utilisateur) throws BusinessException;
-
-
-
-}
->>>>>>> branch 'enchere' of https://github.com/elodiebellec/ProjetTroc.git
