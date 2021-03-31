2561defb433738d163e9cc3b9bc1c2c609615a40
MathisRoyer <mathisroyer.pro@gmail.com>
2021-03-31 14:49:49 +0200

    mise à jour utilisateur doa

--- ProjetTrocMathis/src/fr/eni/projettroc/dao/UtilisateurDAO.java
+++ ProjetTrocMathis/src/fr/eni/projettroc/dao/UtilisateurDAO.java
@@ -10,9 +10,8 @@ public interface UtilisateurDAO {
 public Utilisateur find(String pseudo, String mot_de_passe, String email)throws BusinessException;
 
 public Utilisateur selectByNoUtilisateur(int no_utilisateur)throws BusinessException;
-	
-}
 
- 
-    public Utilisateur selectByPseudo(String pseudo)throws BusinessException;
+public void insertUtilisateur(Utilisateur utilisateur)throws BusinessException;
+	
+public Utilisateur selectByPseudo(String pseudo)throws BusinessException;
 }
