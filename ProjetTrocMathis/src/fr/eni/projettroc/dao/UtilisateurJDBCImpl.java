270bc2f3c0cd154769a04ad195d2fd9e59301507
elodie.b <elodie.bellec@gmail.com>
2021-04-01 22:06:03 +0200

    suppression builderUtilisateur non utilisé

--- ProjetTrocMathis/src/fr/eni/projettroc/dao/UtilisateurJDBCImpl.java
+++ ProjetTrocMathis/src/fr/eni/projettroc/dao/UtilisateurJDBCImpl.java
@@ -163,20 +163,7 @@ public class UtilisateurJDBCImpl implements UtilisateurDAO{
 	}
 
 
-	private Utilisateur builderUtilisateur(ResultSet rs)throws SQLException {
-		Utilisateur utilisateur = new Utilisateur();
-		utilisateur.setNo_utilisateur(rs.getInt("no_utilisateur"));
-		utilisateur.setPseudo(rs.getString("pseudo"));
-		utilisateur.setNom(rs.getString("nom"));
-		utilisateur.setPrenom(rs.getString("prenom"));
-		utilisateur.setEmail(rs.getString("email"));
-		utilisateur.setTelephone(rs.getString("telephone"));
-		utilisateur.setRue(rs.getString("rue"));
-		utilisateur.setCode_postal(rs.getString("code_Postal"));
-		utilisateur.setVille(rs.getString("ville"));
-		utilisateur.setMot_de_passe(rs.getString("mot_de_passe"));
-		return utilisateur;
-	}
+	
 	
 	 public void update(Utilisateur utilisateur)throws BusinessException{
 	       
